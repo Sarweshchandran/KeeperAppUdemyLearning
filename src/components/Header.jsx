4c181b950c7740ef95af35b2bfda71371b4cb2fb
Sarweshchandran <sarweshchandran@gmail.com>
2024-01-25 21:50:46 +0530

    Step-1 Completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+/*Header is Created */
+function Header() {
+    return (
+        <header>
+            <h1>Keeper</h1>
+        </header>
+    );
+}
+
+export default Header;
